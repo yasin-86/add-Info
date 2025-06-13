a7229a7d910abdf585d746ef936f169f4c1f0ebd
Yasin <yasinmen2007@gmail.com>
2025-06-13 17:59:23 +0330

    add-info finished

--- js/app.js
+++ js/app.js
@@ -2,40 +2,63 @@ const signupForm = document.querySelector("#signupForm");
 const studentList = [];
 const tableBody = document.querySelector("#tableBody");
 
-signupForm.addEventListener("submit",(e)=>{
-e.preventDefault();
-    const newStudent = {
-        studentName : e.target.studentName.value,
-        studentLastname : e.target.studentLastname.value,
-        fatherName : e.target.fatherName.value,
-        age : e.target.age.value,
-        grade : e.target.grade.value,
-    }
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newStudent = {
+    studentName: e.target.studentName.value,
+    studentLastname: e.target.studentLastname.value,
+    fatherName: e.target.fatherName.value,
+    age: e.target.age.value,
+    grade: e.target.grade.value,
+  };
 
-    e.target.studentName.value = ""
-    e.target.studentLastname.value = ""
-    e.target.fatherName.value = ""
-    e.target.age.value = ""
-    e.target.grade.value = ""
+  if (newStudent.studentName === "" || newStudent.studentLastname === "" || newStudent.fatherName === "" || newStudent.age === "" || newStudent.grade === "") {
+    alert("لطفا تمام فیلد هارا پر کنید");
+    return;
+  }
 
-    studentList.push(newStudent)
-    renderInUI(studentList,tableBody)
-})
+  e.target.studentName.value = "";
+  e.target.studentLastname.value = "";
+  e.target.fatherName.value = "";
+  e.target.age.value = "";
+  e.target.grade.value = "";
 
-function renderInUI(list,container){
-    container.innerHTML = ""
-    list.forEach((value)=>{
-        const tr = document.createElement("tr");
-        tr.innerHTML = ` <td>${value.studentName}</td>
-                         <td>${value.studentLastname}</td>
-                         <td>${value.fatherName}</td>
-                         <td>${value.age}</td>
-                         <td>${value.grade}</td> `
+  studentList.push(newStudent);
+  renderInUI(studentList, tableBody);
+});
 
+function renderInUI(list, container) {
+  container.innerHTML = "";
+  list.forEach((value) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${value.studentName}</td>
+      <td>${value.studentLastname}</td>
+      <td>${value.fatherName}</td>
+      <td>${value.age}</td>
+      <td>${value.grade}</td>
+      <td>
+        <i class="fas fa-trash"></i>
+      </td>
+    `;
 
-        console.log(value)
-        console.log(tr)
-
-        container.appendChild(tr)
-    })
+    container.appendChild(tr);
+  });
 }
+
+// tableBody.addEventListener("click", (e) => {
+//     if (e.target.classList.contains("fa-trash")) {
+//         e.target.parentElement.parentElement.remove();
+//     }
+// });
+
+tableBody.addEventListener("click", (e) => {
+    if (e.target.classList.contains("fa-trash")) {
+        const tr = e.target.closest("tr");
+        const index = tr.rowIndex - 1;
+
+        studentList.splice(index, 1);
+        tr.remove();
+    }
+});
+
